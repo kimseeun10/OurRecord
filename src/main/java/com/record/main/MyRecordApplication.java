33a667cd575f33b6e18061e4328832d9bb53de50
kimseeun10 <tpdms981@gmail.com>
2023-11-28 01:33:09 +0900

    project start

---
+++ src/main/java/com/record/main/MyRecordApplication.java
@@ -0,0 +1,13 @@
+package com.record.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRecordApplication.class, args);
+	}
+
+}
