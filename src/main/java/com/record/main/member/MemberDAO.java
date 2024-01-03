30795292dd6f7135f2c9ba6f63db0e5b1703d712
kimseeun10 <tpdms981@gmail.com>
2024-01-04 00:49:11 +0900

    240103

--- src/main/java/com/record/main/member/MemberDAO.java
+++ src/main/java/com/record/main/member/MemberDAO.java
@@ -5,4 +5,10 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface MemberDAO {
 
+	// 멤버리스트 조회 select
+	public MemberVO getMember(MemberVO memberVO)throws Exception;
+	
+	// 회원가입 add
+	public int setJoin(MemberVO memberVO)throws Exception;
+	
 }
